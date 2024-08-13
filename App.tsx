8343f6f1f6a0a117a89ccb0838b4f1a04b1ec3e0
Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
2024-08-13 12:21:11 +0300

    delete fonts warns

--- App.tsx
+++ App.tsx
@@ -2,13 +2,22 @@ import { StyleSheet } from "react-native";
 import FadeInView from "./src/widgets/FadeWrapper/FadeInView";
 import Navigation from "./src/app/navigation/Navigation";
 import { loadFonts } from "./assets/fonts/Fonts";
-import { useEffect } from "react";
+import { useLayoutEffect, useState } from "react";
+import * as Font from "expo-font";
 
 export default function App() {
-  
-  useEffect(() => {
-    loadFonts();
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useLayoutEffect(() => {
+    (async function () {
+      await Font.loadAsync(loadFonts);
+      setFontsLoaded(true);
+    })()
   }, []);
+
+  if (!fontsLoaded) {
+    return null; 
+  }
 
   return (
     <FadeInView style={styles.fadeInView}>
--- assets/fonts/Fonts.tsx
+++ assets/fonts/Fonts.tsx
@@ -1,10 +1,6 @@
-import * as Font from "expo-font";
-
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    OutfitRegular: require("./Outfit/Outfit-Regular.ttf"),
-    OutfitBold: require("./Outfit/Outfit-Bold.ttf"),
-    OutfitExtraBold: require("./Outfit/Outfit-ExtraBold.ttf"),
-    InterRegular: require("./Inter/Inter-Regular.ttf"),
-  });
+export const loadFonts = {
+  OutfitRegular: require("./Outfit/Outfit-Regular.ttf"),
+  OutfitBold: require("./Outfit/Outfit-Bold.ttf"),
+  OutfitExtraBold: require("./Outfit/Outfit-ExtraBold.ttf"),
+  InterRegular: require("./Inter/Inter-Regular.ttf"),
 };
